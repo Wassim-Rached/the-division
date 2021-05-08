e5cc5026d090e38822fcf084a48dac04c3e428f5
Wassim <wassimrached404@gmail.com>
2021-05-08 23:50:36 +0100

    add second day work (fi lil XD) 3hours work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 window.addEventListener("scroll",function () {
     const header = document.querySelector('header');
+    const about = this.document.getElementById("about")
+    const discord = this.document.getElementById("discord")
+
+    about.classList.toggle("move", window.scrollY > 0);
     header.classList.toggle("sticky", window.scrollY > 0);
+    discord.classList.toggle("move", window.scrollY > 0);
 });
