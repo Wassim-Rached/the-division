deace66e3dd50a554016816e467cc1e757cc9286
Wassim <wassimrached404@gmail.com>
2021-05-09 20:06:26 +0100

    add second day work (4h)

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ window.addEventListener("scroll",function () {
     const header = document.querySelector('header');
     const about = this.document.getElementById("about")
     const discord = this.document.getElementById("discord")
+    const scroll = this.document.getElementById("scroll")
 
     about.classList.toggle("move", window.scrollY > 0);
     header.classList.toggle("sticky", window.scrollY > 0);
     discord.classList.toggle("move", window.scrollY > 0);
+    scroll.classList.toggle("visible", window.scrollY > 0);
 });
+
+let loading =document.querySelector('.loading');
+window.addEventListener('load',function () {
+    loading.style.display = "none"
+})
